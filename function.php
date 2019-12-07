814bb39896c9760a6e5866d91e14b22d24e136d4
Andrew-1000 <andruambia95@gmail.com>
2019-12-08 00:18:06 +0300

    Provide the MailChip API Credentials to the function.php file like API Key and List ID

--- function.php
+++ function.php
@@ -4,4 +4,11 @@ if(isset($_POST['submit'])){
   $names = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
+  if(!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+    //Providing API Credentials
+    $apiKey = 'e608817098f3dc675bd2cac03ae0f410-us4';
+    $listID = 'd737bb660e';
+
+    //M
+  }
 }
