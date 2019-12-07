0b1bf66b2c936634fc1b3b20c0e207a19e767d03
Andrew-1000 <andruambia95@gmail.com>
2019-12-07 19:46:41 +0300

    Create new style in the css folder to work on hover effects for images

--- js/script.js
+++ js/script.js
@@ -14,5 +14,30 @@ $(document).ready (function(){
   $(".toggleDevLogo").click(function(){
     $(".toggleDevLogo").hide();
     $("#Develop").show();
-  })
+  });
+  $("#productLogo").click(function(){
+    $("#Product").hide();
+    $(".toggleProductLogo").show();
+  });
+  $(".toggleProductLogo").click(function(){
+    $(".toggleProductLogo").hide();
+    $("#Product").show();
+  });
+  //Function for Hovering on images
+  $(function() {
+    var mobileScreenTreshold = 1024;
+    $(".hvrbox").click(function(e) {
+      if($(window).width() <= mobileScreenTreshold) {
+        if($(this).hasClass("active")) {
+          $(this).removeClass("active");
+        } else {
+          e.preventDefault();
+          $(this).addClass("active");
+        }
+      } else {
+        $(this).removeClass("active");
+      }
+    });
+  });
+  
 });
